69c09a88d106c958712fa19cc6f53188115ada59
maramkhaled <maramraslan45@gmail.com>
2022-03-12 17:07:37 +0200

    d

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicPath = path.join(__dirname, 'build', 'index.html');
+const publicPath = path.join( 'build', 'index.html');
 const port = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join( 'build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(publicPath));
  });
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
